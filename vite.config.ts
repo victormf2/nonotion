820a1f6bff3737e9c4c10334a81281234a9e2b73
Victor Machado <victor_mf2@hotmail.com>
2025-12-09 07:16:29 -0300

    lots of PWA stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,35 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/nonotion/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: process.env.CI === 'true' ? false : true,
+      },
+      manifest: {
+        name: 'nonotion',
+        short_name: 'nonotion',
+        description: 'nonotion add notions to your notion',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'nonotion-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'nonotion-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
